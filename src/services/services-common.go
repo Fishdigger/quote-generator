15daa3d15aac7126b472d7b834d6a1037f64bc2a
Doug Almena <ddalmena@gmail.com>
2017-12-30 02:33:37 -0500

    fix response type to json

--- src/services/services-common.go
+++ src/services/services-common.go
@@ -6,8 +6,8 @@ import (
 )
 
 func respondWithJSON(w http.ResponseWriter, v interface{}) {
-	json.NewEncoder(w).Encode(v)
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(v)
 }
